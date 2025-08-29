56f17c51d3fadc34d3b391a6789bdef55c9ddf4c
BBBenson <bbbenson26@gmail.com>
2025-08-29 15:40:55 +0000

    index update with sytle

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -18,39 +18,13 @@ export default function Index() {
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: "#90caf9",
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     padding: 20,
-//   },
-//   largeText: {
-//     fontSize: 30,
-//     color: "#f44336",
-//     marginBottom: 5,
-//     fontWeight: "700",
-//     fontVariant: ["small-caps"],
-//   },
-//   mediumText: {
-//     fontSize: 20,
-//     color: "#9c27b0",
-//     marginBottom: 10,
-//     fontWeight: "500",
-//     textAlign: "right",
-//   },
-//   smallText: {
-//     fontSize: 15,
-//     color: "#2196f3",
-//     fontWeight: "400",
-//     textAlign: "center",
-//   },
-// });
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
   },
   largeText: {
     fontSize: 30,
@@ -73,3 +47,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+
